7dcccfd523cc06b898182f6794cbe2513a02b18e
Nguyễn Thành Tâm <141797848+0337742075@users.noreply.github.com>
2024-08-03 21:17:50 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SignInSide from './pages/SignIn';
@@ -7,24 +6,13 @@ import Staff from './pages/Staff';
 import CheckOut from './pages/CheckOut';
 import Home from './pages/Home';
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import SignInSide from "./pages/SignIn";
-import Manager from "./pages/Manager";
-import Staff from "./pages/Staff";
-import CheckOut from "./pages/CheckOut";
-import Bills from "./pages/Bills";
-import PaymentSuccess from "./pages/PaymentSuccess";
-import { Create } from "@mui/icons-material";
-import CreateBill from "./pages/CreateBill";
 function App() {
   return (
     <>
-      <BrowserRouter>
+     <BrowserRouter>
         <Routes>
-          {localStorage.getItem("token") && (
+          {localStorage.getItem('token')&& (
             <>
-
               {localStorage.getItem('role') && localStorage.getItem('role')=='manager' && (
                     <> 
                       <Route path='/manager' element={<Manager/>}/>
@@ -34,29 +22,14 @@ function App() {
               )}
               {localStorage.getItem('role') && localStorage.getItem('role')=='staff' && (
                       <Route path='/nhan-vien' element={<Staff/>}/>
-                      // ,<Route path='/nhan-vien' element={<Home/>}/> BarChar
+                      ,<Route path='/nhan-vien' element={<Home/>}/>
                     )}
             </>
           )}
                   <Route path='/' element={<SignInSide/>}/>
                   <Route path='*' element={<SignInSide/>}/>
-              {localStorage.getItem("role") && localStorage.getItem("role") == "manager" && (
-                <>
-                  <Route path="/manager" element={<Manager />} />
-                  <Route path="/thanh-toan" element={<CheckOut />} />
-                  <Route path="/pagementsuccess" element={<PaymentSuccess />} />
-                  <Route path="/tao-bill" element={<CreateBill />} />
-                  <Route path="/bills/:id" element={<Bills />} />
-                </>
-              )}
-              {localStorage.getItem("role") && localStorage.getItem("role") == "staff" && <Route path="/nhan-vien" element={<Staff />} />}
-            </>
-          )}
-          <Route path="/" element={<SignInSide />} />
-          <Route path="*" element={<SignInSide />} />
-
         </Routes>
-      </BrowserRouter>
+     </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { Link } from 'react-router-dom';
 function Navbar() {
@@ -59,61 +58,6 @@ function Navbar() {
 
     </>
   )
-import React from "react";
-import { Link } from "react-router-dom";
-
-function Navbar() {
-	const Logout = () => {
-		localStorage.clear();
-		window.location.replace("/");
-	};
-	return (
-		<>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<div className="container-fluid">
-					<Link className="navbar-brand" to="#">
-						<img src="https://marketplace.canva.com/EAE85VgPq3E/1/0/1600w/canva-v%E1%BA%BD-tay-h%C3%ACnh-tr%C3%B2n-logo-c3Jw1yOiXJw.jpg" className="rounded-circle" style={{ height: "50px" }} alt="" />
-					</Link>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon" />
-					</button>
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<Link className="nav-link active" aria-current="page" to="/manager">
-									Trang chủ
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link className="nav-link active" aria-current="page" to="/thanh-toan">
-									Thanh toán
-								</Link>
-							</li>
-							<li className="nav-item">
-								<Link className="nav-link active" aria-current="page" to="#" onClick={(e) => Logout()}>
-									Đăng xuất
-								</Link>
-							</li>
-						</ul>
-						<form className="d-flex">
-							<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-							<button className="btn btn-outline-success" type="submit">
-								Search
-							</button>
-						</form>
-					</div>
-				</div>
-			</nav>
-		</>
-	);
-
 }
 
-export default Navbar;
+export default Navbar

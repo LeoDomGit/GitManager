c2615879e3de7b7a1c67a71a088c14a98ee08a65
Nguyễn Thành Tâm <141797848+0337742075@users.noreply.github.com>
2024-08-05 20:33:45 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SignInSide from './pages/SignIn';
@@ -32,52 +31,6 @@ function App() {
         </Routes>
      </BrowserRouter>
     </>
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import SignInSide from "./pages/SignIn";
-import Manager from "./pages/Manager";
-import Staff from "./pages/Staff";
-import CheckOut from "./pages/CheckOut";
-import Bills from "./pages/Bills";
-import PaymentSuccess from "./pages/PaymentSuccess";
-import CreateBill from "./pages/CreateBill";
-import Home from "./pages/Home";
-
-function App() {
-  const token = localStorage.getItem("token");
-  const role = localStorage.getItem("role");
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        {token ? (
-          <>
-            {role === "manager" && (
-              <>
-                <Route path="/bao-cao" element={<Home />} />
-                <Route path="/manager" element={<Manager />} />
-                <Route path="/thanh-toan" element={<CheckOut />} />
-                <Route path="/payment-success" element={<PaymentSuccess />} />
-                <Route path="/tao-bill" element={<CreateBill />} />
-                <Route path="/bills/:id" element={<Bills />} />
-              </>
-            )}
-            {role === "staff" && (
-              <>
-                <Route path="/nhan-vien" element={<Staff />} />
-              </>
-            )}
-          </>
-        ) : (
-          <>
-            <Route path="/" element={<SignInSide />} />
-            <Route path="*" element={<SignInSide />} />
-          </>
-        )}
-      </Routes>
-    </BrowserRouter>
-
   );
 }
 
